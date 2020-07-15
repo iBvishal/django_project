41e68ce52bc1d513b3392b4eba050ec24134e0f4
iBvishal <bhardwaj.vishalnit@outlook.com>
2020-07-15 22:11:09 +0530

    creating and testing UserModel

---
+++ app/core/test/test_models.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_sucessfull(self):
+        """Test Creating a new user with email is sucessfull"""
+        email = 'test@email.com'
+        password = 'test@123'
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+    
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@email.com'
+        user = get_user_model().objects.create_user(email, 'test@123')
+
+        self.assertEqual(user.email, email.lower())
+    
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raise error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test@123')
